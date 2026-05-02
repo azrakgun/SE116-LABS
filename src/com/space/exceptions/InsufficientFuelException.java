944beda297f1119e85a5b5afcfd5ee40fc2070fe
Azra Akgün <akgunazra2004@gmail.com>
2026-05-02 03:30:14 +0300

    Delete src/com/space directory

--- src/com/space/exceptions/InsufficientFuelException.java
+++
@@ -1,9 +0,0 @@
-package com.space.exceptions;
-import com.space.exceptions.InsufficientFuelException;
-import com.space.mission.MissionTask;
-
-public class InsufficientFuelException extends Exception {
-    public InsufficientFuelException() {
-        super("Not enough fuel for the mission task");
-    }
-}
